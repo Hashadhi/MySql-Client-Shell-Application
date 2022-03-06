1eaf8211b1f8986cad47fe4d6b8c0fd9c961ce92
Hashadhi <hashadhijayasinghe@gmail.com>
2022-03-06 19:58:25 +0530

    Validate user inputs

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -15,6 +16,32 @@ public class LoginFormController {
     public TextField txtHost;
 
     public void btnConnect_OnAction(ActionEvent actionEvent) {
+
+
+    }
+
+    private boolean isValidInputs() {
+        String username = txtUserName.getText().trim();
+        if (username.equals("")) {
+            new Alert(Alert.AlertType.ERROR, "Username cannot be empty!").show();
+            txtUserName.requestFocus();
+            return false;
+        } else if (!username.matches("[A-Za-z0-9]+")) {
+            new Alert(Alert.AlertType.ERROR, "Enter a valid username").show();
+            txtUserName.selectAll();
+            return false;
+        } else if (txtPassword.getText().trim().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Password cannot be empty").show();
+            txtPassword.requestFocus();
+            return false;
+        } else if (txtHost.getText().isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Host cannot be empty").show();
+            txtHost.requestFocus();
+            return false;
+        } else if (!txtPort.getText().matches("\\d+")) {
+            new Alert(Alert.AlertType.ERROR, "Invalid port!").show();
+        }
+        return true;
     }
 
     public void btnExit_OnAction(ActionEvent actionEvent) {((Stage) btnConnect.getScene().getWindow()).close();}
